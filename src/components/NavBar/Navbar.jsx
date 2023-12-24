79dc6e1c51d3e8964c96c56fd3a50586e0458e16
Dasi012 <dastandulatbekuulu@gmail.com>
2023-12-24 17:58:59 +0600

    fixed components

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -10,6 +10,7 @@ function NavBar() {
 				<motion.span
 					initial={{ opacity: 0, scale: 0.5 }}
 					animate={{ opacity: 1, scale: 1 }}
+					transition={{ duration: 0.5 }}
 					className='wrapper'>
 					<span>Lama Dev</span>
 					<div className='social'>
